82be80261889582d1f82787e61e88d1caff69bad
Samruddhi Rajole <samrajole2@gmail.com>
2024-02-29 01:23:20 +0530

    first commit

---
+++ src/main/java/demo_maven/grade.java
@@ -0,0 +1,35 @@
+package demo_maven;
+
+public class grade {
+
+	public static char showgrade(int percentage)
+
+	{
+
+		if (percentage < 0)
+
+			throw new IllegalArgumentException("Number grade cannot be 0");
+
+		else if (percentage < 60)
+
+			return 'F';
+
+		else if (percentage < 70)
+
+			return 'D';
+
+		else if (percentage < 80)
+
+			return 'C';
+
+		else if (percentage < 90)
+
+			return 'B';
+
+		else
+
+			return 'A';
+
+	}
+
+}
